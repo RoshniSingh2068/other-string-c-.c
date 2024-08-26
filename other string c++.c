9457e6ae729ea82e7236ae38deed577fc294f0df
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 11:10:27 +0530

    Create other string c++.c

---
+++ other string c++.c
@@ -0,0 +1,10 @@
+// the sequence\' insert a single quote in the string
+
+#include <iostream>
+
+using namespace std;
+int main(){
+    string txt = "it is alright \'no its not\' .";
+    cout << txt;
+    return 0;
+}
